754ab59fc4af91430fe3f151a71c2a67f870592c
gountz <76885512+gountz@users.noreply.github.com>
2021-06-10 00:04:56 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const añadir = document.getElementById('add');
+const campoDeTexto = document.getElementById('campo-de-texto')
+const lista = document.getElementById('notes');
+const deleter = document.getElementsByTagName('li');
+
+añadir.onclick = ()=>{
+    const contenido = "<li><i onclick='func(this)' class='bx bx-checkbox'></i>"+ campoDeTexto.value+"<i onclick='eliminarElemento(this)' class='bx bx-trash'></i></li>"
+    lista.insertAdjacentHTML('beforeend',contenido);
+}
+
+function eliminarElemento(elemento){
+    elemento.parentElement.remove();
+    
+}
+function func($kiwea){
+    $kiwea.classList.toggle('bx-checkbox');
+    $kiwea.classList.toggle('bxs-checkbox-checked');
+  
+}
